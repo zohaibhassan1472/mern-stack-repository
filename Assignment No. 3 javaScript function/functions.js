25992e6cb7badad82ac5d19a8c161ae568fbe581
zohaibhassan1472 <zohaib1472@gmail.com>
2025-07-24 18:56:29 +0500

    first question complete

--- Assignment No. 3 javaScript function/functions.js
+++ Assignment No. 3 javaScript function/functions.js
@@ -1,14 +1,12 @@
 // Greeting Function: Create a function greet(name) that takes a name as an argument 
 // and returns a personalized greeting message.
-
-let myname="zohaib";
-
 function greet(name) {
-    
+    console.log("event listener run")
     return name;
 };
-console.log(greet(myname));
-
-let s1=document.getElementById("question_first").value;
-let s2=document.getElementById("write_name");
-s2.innerHTML=s1;
+document.getElementById("press_greeting").addEventListener("click", function () {
+    let fullname = document.getElementById("question_first").value;
+    let s2 = document.getElementById("write_name");
+    s2.innerHTML = greet(fullname);
+});
+// Calculator Function: Create a function calculateSum(a, b) that takes two numbers as arguments and returns their sum.

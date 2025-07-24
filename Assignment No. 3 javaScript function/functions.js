e68c990186d4665177b33bde94b57ce6de4e8037
zohaibhassan1472 <zohaib1472@gmail.com>
2025-07-24 20:03:02 +0500

    question no 3 soled

--- Assignment No. 3 javaScript function/functions.js
+++ Assignment No. 3 javaScript function/functions.js
@@ -10,3 +10,25 @@ document.getElementById("press_greeting").addEventListener("click", function () 
     s2.innerHTML = greet(fullname);
 });
 // Calculator Function: Create a function calculateSum(a, b) that takes two numbers as arguments and returns their sum.
+document.getElementById("sum").addEventListener("click", function () {
+    let number_1 = +document.getElementById("num_1").value;
+    let number_2 = +document.getElementById("num_2").value;
+    document.getElementById("sum_result").innerHTML = number_1 + number_2;
+    console.log("zohaib")
+});
+
+// Even or Odd Function: Create a function isEven(num) that takes a number as an argument and 
+// returns true if the number is even, and false otherwise.
+
+function isEven() {
+    let even_odd = +document.getElementById("even_odd").value;
+    let check = even_odd % 2;
+    console.log(check)
+    if (check == 0) {
+        document.getElementById("even_odd_result").innerHTML = "true";
+    } else {
+        document.getElementById("even_odd_result").innerHTML = "false";
+    }
+}
+document.getElementById("identify").addEventListener("click", isEven);
+

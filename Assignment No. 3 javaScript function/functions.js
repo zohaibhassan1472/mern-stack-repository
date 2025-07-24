1bf1dbc694c02a21ef8616641aef68275eb8bbd1
zohaibhassan1472 <zohaib1472@gmail.com>
2025-07-24 17:04:12 +0500

    first question in process

--- Assignment No. 3 javaScript function/functions.js
+++ Assignment No. 3 javaScript function/functions.js
@@ -1,3 +1,14 @@
-let a=20;
-console.log("HELLOW ZOHAIB");
-console.log("HELLOW khan");
+// Greeting Function: Create a function greet(name) that takes a name as an argument 
+// and returns a personalized greeting message.
+
+let myname="zohaib";
+
+function greet(name) {
+    
+    return name;
+};
+console.log(greet(myname));
+
+let s1=document.getElementById("question_first").value;
+let s2=document.getElementById("write_name");
+s2.innerHTML=s1;

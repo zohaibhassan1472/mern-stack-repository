0f35cf6cf354c8881bcd6ac4403fa47db4b77d3c
zohaibhassan1472 <zohaib1472@gmail.com>
2025-07-26 18:33:33 +0500

    question 9 completed

--- Assignment No. 3 javaScript function/functions.js
+++ Assignment No. 3 javaScript function/functions.js
@@ -38,11 +38,11 @@ document.getElementById("identify").addEventListener("click", isEven);
 function repeatString() {
     let user_string = document.getElementById("string").value;
     let user_num = +document.getElementById("num").value;
-    let show_repeater=document.getElementById("repeats");
+    let show_repeater = document.getElementById("repeats");
     show_repeater.innerHTML = "";
-   
-    
-    for(let i=1; i<=user_num; i++ ){
+
+
+    for (let i = 1; i <= user_num; i++) {
         show_repeater.append(`${i}:   ${user_string} `);
 
     };
@@ -52,3 +52,96 @@ document.getElementById("repeater").addEventListener("click", repeatString);
 
 // Temperature Converter Function: Create a function convertCelsiusToFahrenheit(celsius) 
 // that takes a temperature in Celsius as an argument and returns the equivalent temperature in Fahrenheit.
+
+function convertCelsiusToFahrenheit() {
+    let get_celsius = +document.getElementById("celsius").value;
+    fahrenheit = get_celsius * 9 / 5 + 32;
+    console.log(fahrenheit);
+    document.getElementById("fahrenheit").innerHTML = fahrenheit;
+
+}
+document.getElementById("convert").addEventListener("click", convertCelsiusToFahrenheit);
+
+
+// Age Calculator Function: Create a function calculateAge(birthYear) that takes a birth year 
+// as an argument and returns the person's age.
+
+function calculateAge() {
+    const d = new Date();
+    let year = d.getFullYear();
+    return year;
+}
+document.getElementById("calculate").addEventListener("click", function () {
+    let current_age = +document.getElementById("current_age").value;
+    document.getElementById("your_age").innerHTML = calculateAge() - current_age;
+})
+
+// Random Number Generator Function: Create a function generateRandomNumber(min, max) 
+// that takes two numbers as arguments and returns a random number between min and max.
+
+function generateRandomNumber() {
+    let get_min_num = +document.getElementById("min_num").value;
+    let get_max_num = +document.getElementById("max_num").value;
+    return Math.floor(Math.random() * (get_max_num - get_min_num) + get_min_num);
+
+}
+
+document.getElementById("generate_random").addEventListener("click", function () {
+    document.getElementById("random_num_generated").innerHTML = generateRandomNumber();
+})
+
+// String Length Function: Create a function getStringLength(str) 
+// that takes a string as an argument and returns its length.
+
+function getStringLength(str) {
+    let conv_character = str.split("");
+    return conv_character.length;
+}
+document.getElementById("counter").addEventListener("click", function () {
+    let get_user_string = document.getElementById("user_string").value;
+    document.getElementById("show_lenght").innerHTML = getStringLength(get_user_string);
+})
+
+
+// Array Sum Function: Create a function calculateArraySum(arr) 
+// that takes an array of numbers as an argument and returns their sum.
+
+let get_user_number = document.getElementById("user_numbers");
+
+
+get_user_number.addEventListener("keydown", function (e) {
+    if (
+        e.key === "Backspace" ||
+        e.key === "Delete" ||
+        e.key === "Tab" ||
+        e.key === "ArrowLeft" ||
+        e.key === "ArrowRight" ||
+        e.key === " "
+    ) {
+        return;
+    }
+    let get_user = document.getElementById("user_numbers").value;
+    console.log(get_user)
+
+    if (!/^[0-9]$/.test(e.key)) {
+
+        e.preventDefault();
+    }
+
+});
+
+document.getElementById("sum_user_numers").addEventListener("click", function () {
+    let get_user_all_numbers = document.getElementById("user_numbers").value;
+    let conv_str_int = get_user_all_numbers.split(" ");
+    console.log(conv_str_int)
+    let sum =0;
+    for (let i = 0; i < conv_str_int.length; i++) {
+        sum=sum+parseInt(conv_str_int[i])
+        
+    }
+    document.getElementById("show_sum_number").innerHTML=sum;
+})
+
+
+// Vowel Counter Function: Create a function countVowels(str) that takes a string as an argument 
+// and returns the number of vowels in the string.
